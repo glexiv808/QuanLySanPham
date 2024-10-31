13843fab1b0a718042be112ae2423b2c2d147dc6
nguyen <125894255+glexiv808@users.noreply.github.com>
2024-11-01 00:32:10 +0700

    Initial commit

---
+++ src/test/java/com/BTL/QuanLySanPham/QuanLySanPhamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BTL.QuanLySanPham;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuanLySanPhamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
